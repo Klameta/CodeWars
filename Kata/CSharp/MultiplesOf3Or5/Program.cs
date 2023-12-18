b94eb8f95b8649eb4a57b677210d8c5232f606e2
Klameta <ivanavalcheva@gmail.com>
2023-12-18 18:30:55 +0200

    created the repository for my CodeWars exercises. Hopefully i use this.

---
+++ Kata/CSharp/MultiplesOf3Or5/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine(Kata.Solution(10));
+
+public static class Kata
+{
+    public static int Solution(int value)
+    {
+        value--;
+        int sumOfMultiples = 0;
+        while (value >= 3)
+        {
+            if (value % 3 == 0 || value % 5 == 0)
+            {
+                sumOfMultiples += value;
+            }
+            value--;
+        }
+
+        return sumOfMultiples;
+    }
+}
---
+++ Kata/CSharp/UniqueInORder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+int[] arr = {1, 2, 2, 3, 3};
+Console.WriteLine("."+string.Join("", Kata.UniqueInOrder(arr))+".");
+public static class Kata
+{
+    public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+    {
+        var filtred = iterable
+            .Select((num, index) => new
+                {
+                    Current = num,
+                    Next = iterable.ElementAtOrDefault(index + 1)
+                })
+            .Where(pair => !EqualityComparer<T>.Default.Equals(pair.Current, pair.Next))
+            .Select(pair => pair.Current);
+
+        return filtred;
+
+    }
+}
